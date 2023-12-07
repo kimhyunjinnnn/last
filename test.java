b51308349abefb256c25af84088a63dfcca9a268
hyunjin kim <hjazure19@gmail.com>
2023-12-08 00:16:21 +0900

    vars

--- test.java
+++ test.java
@@ -1,5 +1,7 @@
 package github;
 
+import java.util.Scanner;
+
 class Food{
 	int price;
 	int bonusPoint;
@@ -26,6 +28,40 @@ class Coke extends Food{
 		super(1000);   //콜라를 1000원으로 한다
 	}
 	public String toString() {return "코카콜라";}//Object클래스의 toString()을 오버라이딩
+}
+class Buyer{
+	Scanner s = new Scanner(System.in);
+	int price;
+	int bonusPoint;
+	int count=0;
+	int foodCnt;
+	int foodCnt2;
+	Food[] cart = new Food[10];
+	Integer[] foodAmount = new Integer[10];
+    int cartCnt =0;
+	int buy(Food selectedFood) {
+		if(price==0) { //장바구니 비우면 카트초기화
+			cartCnt=0; 
+			foodCnt=0;
+			foodCnt2=0;
+		}
+		System.out.println("수량 : ");
+		foodAmount[foodCnt++] = s.nextInt(); //수량 입력
+		System.out.println(selectedFood+" "+foodAmount[foodCnt2]+"개 장바구니에 담았습니다. ");
+		price+=selectedFood.price*foodAmount[foodCnt2]; 
+		bonusPoint+=selectedFood.bonusPoint*foodAmount[foodCnt2];
+		System.out.println("결제 금액 : "+selectedFood.price*foodAmount[foodCnt2]);
+		System.out.println("적립금 : "+selectedFood.bonusPoint*foodAmount[foodCnt2]);
+		cart[cartCnt++]=selectedFood;
+		foodCnt2++;
+		return selectedFood.price; //선택한 상품 단가 리턴
+	}
+	int sumPrice() { //총금액리턴
+		return price;
+	}
+	int sumBonusPoint() { //총적립금리턴
+		return bonusPoint;
+	}
 }
 
 public class test {
