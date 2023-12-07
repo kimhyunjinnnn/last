8937e6eabd89e832e7f3440dc56854694073a9bb
hyunjin kim <hjazure19@gmail.com>
2023-12-08 00:11:51 +0900

    Shrimpburger

--- test.java
+++ test.java
@@ -15,6 +15,12 @@ class Cheeseburger extends Food{
 	}
 	public String toString() {return "치즈버거";}//Object클래스의 toString()을 오버라이딩
 }
+class Shrimpburger extends Food{
+	Shrimpburger(){  //조상클래스의 생성자 Food(int price)를 호출
+		super(4000);   //새우버거를 4000원으로 한다
+	}
+	public String toString() {return "새우버거";}//Object클래스의 toString()을 오버라이딩
+}
 
 public class test {
 
