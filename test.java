f5ef6a1d5cfafa3522830004cb026379360a02f8
hyunjin kim <hjazure19@gmail.com>
2023-12-08 00:11:23 +0900

    Cheeseburger

--- test.java
+++ test.java
@@ -1,8 +1,25 @@
 package github;
 
+class Food{
+	int price;
+	int bonusPoint;
+	
+	Food(int price){
+		this.price=price;
+		bonusPoint=(int)(price/10);
+	}
+}
+class Cheeseburger extends Food{
+	Cheeseburger(){  //조상클래스의 생성자 Food(int price)를 호출
+		super(4000);   //치즈버거를 4000원으로 한다
+	}
+	public String toString() {return "치즈버거";}//Object클래스의 toString()을 오버라이딩
+}
+
 public class test {
 
 	public static void main(String[] args) {
 	}
 
 }
+
